e31442a1ffe3cbc484b7615d57aaff0f67dde0c5
Valerio Formato <valerio.formato@gmail.com>
2015-02-16 10:17:11 +0100

    Adding doxygen config file

--- test/test_galprop/src/test.cpp
+++ test/test_galprop/src/test.cpp
@@ -1,9 +1,23 @@
 #include </storage/gpfs_ams/ams/users/vformato/devel/PropManager/source/GalpropManager/include/GalpropManager.h>
 
+Interface* obj = NULL;
+
 int main(){
 
   GalpropManager* mgr = new GalpropManager();
   mgr->Print();
   mgr->Run();
+
+  obj = new Interface();
+
+
+}
+
+Double_t ModelFlux( Double_t x, Double_t delta ){
+
+  if( obj->GetDelta() != delta )
+    obj->Run();
+
+  return obj->GetFlux(1, 1)->Eval(;
 
 }
